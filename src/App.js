8aac52d72c735cb77cf309567953cb03e1428d4d
JK1320 <kaurjus1@gmail.com>
2021-12-24 14:26:58 +0000

    Spell checker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
+import {useState, useEffect} from "react";
 import './App.css';
+// import $Spelling from "jquery";
+
+let words = ["come", "get", "give", "go"];
+//   {
+//     id: 0,
+//     word: "come",
+//   },
+//   {
+//     id: 1,
+//     word: "get",
+//   },
+//   {
+//     id: 2,
+//     word: "give",
+//   },
+//   {
+//     id: 3,
+//     word: "go",
+//   },
+// ];
 
 function App() {
+const [searchInput, setSearchInput] = useState("");
+const [options, setOptions] = useState();
+
+
+  useEffect(() => {
+    if (searchInput !== "") {
+      const filteredOptions = words.filter((item) =>
+        item.toLowerCase().includes(searchInput.toLowerCase())
+      );
+      setOptions(filteredOptions);
+    } else if (searchInput === "") {
+      setOptions("");
+    }
+  }, [searchInput]);
+
+
+
+  // function checkSpell() {
+  //   $Spelling.SpellCheckInWindow("textarea");
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Spell Checker</h1>
       </header>
+      <main className="main-div">
+        <div>
+          <textarea
+            className="text1"
+            id="textarea"
+            type="text"
+            value={searchInput}
+            placeholder="please type your text here.."
+            spellCheck="true"
+            onChange={(event) => {
+              setSearchInput(event.target.value);
+            }}
+          ></textarea>
+          {/* <button value="CHECK SPELLING" onClick={checkSpell()}>
+            Check
+          </button> */}
+        </div>
+        <div>
+          <h3>Options:</h3>
+          {options && <p className="text2">{options.map(option => `${option},  `)}</p>}  
+        </div>
+      </main>
     </div>
   );
 }
